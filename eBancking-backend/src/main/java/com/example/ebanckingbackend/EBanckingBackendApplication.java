a5d13208e633fc1e17c8b6d483641c8634c49454
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-05-16 14:18:56 +0100

    create some instances

--- eBancking-backend/src/main/java/com/example/ebanckingbackend/EBanckingBackendApplication.java
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/EBanckingBackendApplication.java
@@ -1,7 +1,20 @@
 package com.example.ebanckingbackend;
 
+import com.example.ebanckingbackend.entities.AccountOperation;
+import com.example.ebanckingbackend.entities.CurrentAccount;
+import com.example.ebanckingbackend.entities.Customer;
+import com.example.ebanckingbackend.entities.SavingAccount;
+import com.example.ebanckingbackend.enums.AccountStatus;
+import com.example.ebanckingbackend.repositories.AccountOperationRepository;
+import com.example.ebanckingbackend.repositories.BankAccountRepository;
+import com.example.ebanckingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EBanckingBackendApplication {
@@ -9,5 +22,34 @@ public class EBanckingBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(EBanckingBackendApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository, BankAccountRepository bankAccountRepository,
+    AccountOperationRepository accountOperationRepository){
+        return args->{
+            Stream.of("med","medo","moha").forEach(name->{
+                Customer customer=new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                customerRepository.save(customer);
+            });
+            customerRepository.findAll().forEach(customer->{
+                //create saving account
+                SavingAccount sa=new SavingAccount();
+                sa.setCustomer(customer);
+                sa.setBalance(Math.random()*95700);
+                sa.setInterestRate(4.5);
+                sa.setCreatedAt(new Date());
+                sa.setStatus(AccountStatus.CREATED);
+                bankAccountRepository.save(sa);
+                //create current account
+                CurrentAccount ca=new CurrentAccount();
+                ca.setCustomer(customer);
+                ca.setBalance(Math.random()*88000);
+                ca.setCreatedAt(new Date());
+                ca.setOverDraft(88000);
+                ca.setStatus(AccountStatus.CREATED);
+                bankAccountRepository.save(ca);
+            });
+        };
+    }
 }
