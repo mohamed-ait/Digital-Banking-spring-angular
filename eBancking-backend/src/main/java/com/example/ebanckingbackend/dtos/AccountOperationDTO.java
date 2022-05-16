cef35e9947ecac28dce6c3ed9402c001efeed6e2
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-05-16 16:26:59 +0100

    create dto classes

---
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,17 @@
+package com.example.ebanckingbackend.dtos;
+import com.example.ebanckingbackend.enums.OperationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
