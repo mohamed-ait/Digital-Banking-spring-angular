d2f1a967a26b134eecac18f69ca0ecc4c3dd944b
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-05-16 15:34:51 +0100

    switch to mySql

--- eBancking-backend/src/main/java/com/example/ebanckingbackend/EBanckingBackendApplication.java
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/EBanckingBackendApplication.java
@@ -5,6 +5,7 @@ import com.example.ebanckingbackend.entities.CurrentAccount;
 import com.example.ebanckingbackend.entities.Customer;
 import com.example.ebanckingbackend.entities.SavingAccount;
 import com.example.ebanckingbackend.enums.AccountStatus;
+import com.example.ebanckingbackend.enums.OperationType;
 import com.example.ebanckingbackend.repositories.AccountOperationRepository;
 import com.example.ebanckingbackend.repositories.BankAccountRepository;
 import com.example.ebanckingbackend.repositories.CustomerRepository;
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -35,6 +37,7 @@ public class EBanckingBackendApplication {
             customerRepository.findAll().forEach(customer->{
                 //create saving account
                 SavingAccount sa=new SavingAccount();
+                sa.setId(UUID.randomUUID().toString());
                 sa.setCustomer(customer);
                 sa.setBalance(Math.random()*95700);
                 sa.setInterestRate(4.5);
@@ -43,12 +46,25 @@ public class EBanckingBackendApplication {
                 bankAccountRepository.save(sa);
                 //create current account
                 CurrentAccount ca=new CurrentAccount();
+                ca.setId(UUID.randomUUID().toString());
                 ca.setCustomer(customer);
                 ca.setBalance(Math.random()*88000);
                 ca.setCreatedAt(new Date());
                 ca.setOverDraft(88000);
                 ca.setStatus(AccountStatus.CREATED);
                 bankAccountRepository.save(ca);
+                //create 5 operation for each BankAccount
+                bankAccountRepository.findAll().forEach(acc->{
+                    for (int i = 0; i <10 ; i++) {
+                        AccountOperation accountOperation=new AccountOperation();
+                        accountOperation.setOperationDate(new Date());
+                        accountOperation.setAmount(Math.random()*12000);
+                        accountOperation.setType(Math.random()>0.5? OperationType.DEBIT: OperationType.CREDIT);
+                        accountOperation.setBankAccount(acc);
+                        accountOperationRepository.save(accountOperation);
+                    }
+
+                });
             });
         };
     }
