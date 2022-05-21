5f4a5c2fbbb23990eb315193e579036acdd46e4a
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-05-21 20:09:24 +0100

    add angular project

--- eBancking-backend/src/main/java/com/example/ebanckingbackend/repositories/AccountOperationRepository.java
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/repositories/AccountOperationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+    //la liste des opérations d'un compte
     List<AccountOperation> findByBankAccountId(String accountId);
     Page<AccountOperation> findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
 }
