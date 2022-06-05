9d50e1d144743723dc53197239ea388c8b20e618
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-06-05 14:12:02 +0100

    AppRole Repository

---
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/security/entities/repositories/AppRoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.ebanckingbackend.security.entities.repositories;
+
+import com.example.ebanckingbackend.security.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
+
+    AppRoleRepository findByRoleName( String roleName);
+}
