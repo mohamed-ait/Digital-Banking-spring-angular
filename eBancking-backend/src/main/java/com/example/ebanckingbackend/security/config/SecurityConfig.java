890a50be5d1cc31b3e930eb77a557a2af2affa1c
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-06-05 15:52:17 +0100

    JwtAuthorizationFilter

--- eBancking-backend/src/main/java/com/example/ebanckingbackend/security/config/SecurityConfig.java
+++ eBancking-backend/src/main/java/com/example/ebanckingbackend/security/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.example.ebanckingbackend.security.config;
 
 
+import com.example.ebanckingbackend.security.filters.JwtAuthenticationFilter;
+import com.example.ebanckingbackend.security.filters.JwtAuthorizationFilter;
 import com.example.ebanckingbackend.security.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
